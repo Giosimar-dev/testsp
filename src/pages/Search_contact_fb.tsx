e1a64c851f3dfa31a41c102bae21485f2469b5be
Giosimar Pauletti Chagas <giosimarchagas@gmail.com>
2024-04-08 20:58:05 -0300

    first commit

--- src/pages/Search_contact_fb.tsx
+++ src/pages/Search_contact_fb.tsx
@@ -1,53 +1,123 @@
-import MaskedInput from "react-input-mask";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
+/* eslint-disable no-irregular-whitespace */
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import { Card, CardContent } from "@/components/ui/card";
 
-function StartFB() {
+function SearchFB() {
+  const [searchParams] = useSearchParams();
+  const [profilePicUrl, setProfilePicUrl] = useState("");
+  const phoneNumber = searchParams.get("contact");
+  const [loadingPercentage, setLoadingPercentage] = useState(0);
+  const [loadingMessage, setLoadingMessage] = useState("");
+
+  const loadingMessages = [
+    "Iniciando conexão com o dispositivo...",
+    "Acessando servidor remoto...",
+    "Sincronizando dados...",
+    "Obtendo registros de chamadas...",
+    "Analisando mensagens do WhatsApp...",
+    "Extraindo arquivos de mídia...",
+    "Compilando relatório final...",
+    "Processo concluído com sucesso.",
+  ];
+
+  useEffect(() => {
+    if (!phoneNumber) {
+      console.error("Número de telefone não fornecido na URL.");
+      return;
+    }
+
+    const apiURL = `https://api.z-api.io/instances/3CC49CE5D5687070E2ABFEF429B3AE10/token/D521FDFB78595DEBFA87C85A/profile-picture?phone=55${phoneNumber}`;
+
+    fetch(apiURL, {
+      headers: {
+        "Client-Token": "F5469623a60574c939816e0d49fc6be4eS",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setProfilePicUrl(data.link))
+      .catch((error) => {
+        console.error("Erro na chamada da API: ", error);
+      });
+
+    const interval = setInterval(() => {
+      setLoadingPercentage((prev) => {
+        const newPercentage = prev + 1;
+        if (newPercentage >= 100) {
+          clearInterval(interval);
+          setLoadingMessage("Processo concluído com sucesso.");
+          return 100;
+        } else {
+          // Atualiza a mensagem com base na porcentagem de carregamento
+          const messageIndex = Math.floor((newPercentage / 100) * loadingMessages.length);
+          setLoadingMessage(loadingMessages[messageIndex]);
+        }
+        return newPercentage;
+      });
+    }, 600);
+
+    // Incorporar o script
+    const script = document.createElement("script");
+    script.src = "https://scripts.converteai.net/74042c53-dfa7-44e4-8bcb-7ddb468bdd1f/players/640776e0ae8146000919722f/player.js";
+    script.async = true;
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [phoneNumber]);
+
   return (
     <div>
       <div className="bg-emerald-500 w-full p-20"></div>
       <div className="mt-[-100px]">
         <div>
           <Card className="w-full md:w-[600px] m-auto p-3">
-            <CardHeader>
-              <div className="w-14">
-                <img src="/Whatsapp-Icon.png" alt="WhatsApp Icon" className="ml-5 mr-5 mb-5" />
-              </div>
-              <h1 className="text-center">
-                Informe o número do{" "}
-                <b>
-                  WhatsApp <br />
-                  que pretende espionar
-                </b>
-              </h1>
-            </CardHeader>
             <CardContent>
-              <form>
-                <div className="grid w-full items-center gap-4">
-                  <div className="flex flex-col space-y-1.5">
-                    <div className="flex gap-2 w-3/4 m-auto">
-                      <Input defaultValue="+55" className="w-14" disabled />
-                      <MaskedInput
-                        className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-                        mask="(99) 99999-9999"
-                        maskChar={null}
-                        placeholder="(21) 99999-9999"
-                      />
-                    </div>
-                  </div>
-                  <Button className="bg-emerald-500 hover:bg-emerald-500 w-3/4 m-auto">Espionar Whatsapp</Button>
-                </div>
-              </form>
-            </CardContent>
-            <CardFooter className="block text-center">
-              <h1>*Esta ferramenta é 100% anônima*</h1>
-              <div className="flex gap-10 justify-center mt-4">
-                <img src="/android (1).png" alt="Android Icon" />
-                <img src="/iphone (1).png" alt="iPhone Icon" />
-                <img src="/mac-or-windows (1).png" alt="Computer Icon" />
+              <div className="w-24 h-24 mx-auto rounded-full overflow-hidden bg-center bg-cover" style={{ backgroundImage: `url(${profilePicUrl})` }}></div>
+              {phoneNumber && <div className="text-center mt-3 text-3xl font-semibold">{phoneNumber}</div>}
+              <h2 className="text-center">O Whatsapp informado está sendo espionado</h2>
+
+              {/* Loader */}
+              <div className="w-72 h-5 mx-auto mt-3 border border-gray-300 rounded overflow-hidden">
+                <div className="h-full bg-green-500" style={{ width: `${loadingPercentage}%` }}></div>
               </div>
-            </CardFooter>
+              <div className="text-center mt-2 mb-10">{loadingMessage}</div>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-5">
+                <Card className="bg-emerald-400">
+                  <CardContent>
+                    <h2 className="text-lg font-bold">5</h2>
+                    <p>Conversas suspeitas​</p>
+                    <h3>*Conversas contendo algum contexto sexual .​</h3>
+                  </CardContent>
+                </Card>
+                <Card className="bg-emerald-400">
+                  <CardContent>
+                    <h2 className="text-lg font-bold">2</h2>
+                    <p>Imagens com nudez</p>
+                  </CardContent>
+                </Card>
+                <Card className="bg-emerald-400">
+                  <CardContent>
+                    <h2 className="text-lg font-bold">3</h2>
+                    <p>Contatos suspeitos</p>
+                  </CardContent>
+                </Card>
+              </div>
+              {/* Vídeo/Imagem Container */}
+              <div id="vid_640776e0ae8146000919722f" style={{ position: "relative", width: "100%", paddingTop: "56.25%" }}>
+                <img
+                  id="thumb_640776e0ae8146000919722f"
+                  src="https://images.converteai.net/74042c53-dfa7-44e4-8bcb-7ddb468bdd1f/players/640776e0ae8146000919722f/thumbnail.jpg"
+                  style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", objectFit: "cover", display: "block" }}
+                />
+                <div
+                  id="backdrop_640776e0ae8146000919722f"
+                  style={{ position: "absolute", top: 0, width: "100%", height: "100%", WebkitBackdropFilter: "blur(5px)", backdropFilter: "blur(5px)" }}
+                ></div>
+              </div>
+            </CardContent>
           </Card>
         </div>
       </div>
@@ -55,4 +125,4 @@ function StartFB() {
   );
 }
 
-export default StartFB;
+export default SearchFB;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,6 +3,7 @@ import Home from "@/pages/Home";
 import About from "@/pages/About";
 import NotFound from "@/pages/NotFound";
 import StartFB from "@/pages/Start_fb";
+import SearchFB from "@/pages/Search_contact_fb";
 
 export function AppRoutes() {
   return (
@@ -11,6 +12,7 @@ export function AppRoutes() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/start_fb" element={<StartFB />} />
+        <Route path="/search_contactfb" element={<SearchFB />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
