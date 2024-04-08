c2f942e2865f73b3f92f840a5dd14e468693ab69
Giosimar Pauletti Chagas <giosimarchagas@gmail.com>
2024-04-08 18:12:44 -0300

    first commit

--- src/pages/Start_fb.tsx
+++ src/pages/Start_fb.tsx
@@ -9,7 +9,7 @@ function StartFB() {
       <div className="bg-emerald-500 w-full p-20"></div>
       <div className="mt-[-100px]">
         <div>
-          <Card className="w-[600px] m-auto">
+          <Card className="w-full md:w-[600px] m-auto">
             <CardHeader>
               <img src="/Whatsapp-Icon.png" alt="WhatsApp Icon" className="ml-32 mr-32 mb-5" />
               <h1 className="text-center">
@@ -20,7 +20,7 @@ function StartFB() {
               <form>
                 <div className="grid w-full items-center gap-4">
                   <div className="flex flex-col space-y-1.5">
-                    <div className="flex gap-2 w-2/4 m-auto">
+                    <div className="flex flex-col md:flex-row gap-2 w-full md:w-2/4 m-auto">
                       <Input defaultValue="+55" className="w-14" disabled />
                       <MaskedInput
                         className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
