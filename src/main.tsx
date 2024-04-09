cdf5158fa850baaa11e5a1ab4944db48c0acd77b
Giosimar Pauletti Chagas <giosimarchagas@gmail.com>
2024-04-09 12:33:01 -0300

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppRoutes } from "./routes/routes";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Analytics />
     <AppRoutes />
   </React.StrictMode>
 );
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,14 +4,10 @@ import About from "@/pages/About";
 import NotFound from "@/pages/NotFound";
 import StartFB from "@/pages/Start_fb";
 import SearchFB from "@/pages/Search_contact_fb";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export function AppRoutes() {
   return (
     <Router>
-      <SpeedInsights />
-      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
