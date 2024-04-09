d957aa94c0362976f10d88131594be35119eb761
Giosimar Pauletti Chagas <giosimarchagas@gmail.com>
2024-04-09 11:00:16 -0300

    first commit

--- src/pages/Search_contact_fb.tsx
+++ src/pages/Search_contact_fb.tsx
@@ -1,7 +1,13 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-irregular-whitespace */
 import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { Card, CardContent } from "@/components/ui/card";
+import { AiFillMessage } from "react-icons/ai";
+import { FaImages, FaMapMarkedAlt } from "react-icons/fa";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
+import { IoIosUnlock } from "react-icons/io";
 
 function SearchFB() {
   const [searchParams] = useSearchParams();
@@ -9,6 +15,13 @@ function SearchFB() {
   const phoneNumber = searchParams.get("contact");
   const [loadingPercentage, setLoadingPercentage] = useState(0);
   const [loadingMessage, setLoadingMessage] = useState("");
+  const [suspectConversations, setSuspectConversations] = useState(0);
+  const [nudeImages, setNudeImages] = useState(0);
+  const [suspectLocations, setSuspectLocations] = useState(0);
+
+  const renderValueOrSpinner = (value: number) => {
+    return value === 0 ? <ReloadIcon className="m-auto animate-spin h-5 w-5 text-gray-500" /> : value; // Substitua <Spinner /> pelo seu spinner real
+  };
 
   const loadingMessages = [
     "Iniciando conexão com o dispositivo...",
@@ -20,6 +33,10 @@ function SearchFB() {
     "Compilando relatório final...",
     "Processo concluído com sucesso.",
   ];
+
+  const handleButtonClick = () => {
+    window.location.href = "https://funneltools.com.br/sf/?sfunnel=417&utm_campaign=secretspyperfectpayfb"; // Substitua pela URL desejada
+  };
 
   useEffect(() => {
     if (!phoneNumber) {
@@ -51,21 +68,18 @@ function SearchFB() {
           // Atualiza a mensagem com base na porcentagem de carregamento
           const messageIndex = Math.floor((newPercentage / 100) * loadingMessages.length);
           setLoadingMessage(loadingMessages[messageIndex]);
+
+          if (newPercentage % 90 === 0) {
+            setSuspectConversations(Math.floor(Math.random() * 50 + 1));
+            setNudeImages(Math.floor(Math.random() * 20 + 1));
+            setSuspectLocations(Math.floor(Math.random() * 5 + 1));
+          }
         }
         return newPercentage;
       });
     }, 600);
 
-    // Incorporar o script
-    const script = document.createElement("script");
-    script.src = "https://scripts.converteai.net/74042c53-dfa7-44e4-8bcb-7ddb468bdd1f/players/640776e0ae8146000919722f/player.js";
-    script.async = true;
-    document.head.appendChild(script);
-
-    return () => {
-      document.head.removeChild(script);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    return () => clearInterval(interval);
   }, [phoneNumber]);
 
   return (
@@ -73,49 +87,51 @@ function SearchFB() {
       <div className="bg-emerald-500 w-full p-20"></div>
       <div className="mt-[-100px]">
         <div>
-          <Card className="w-full md:w-[600px] m-auto p-3">
+          <Card className="w-full md:w-[800px] m-auto p-3">
             <CardContent>
               <div className="w-24 h-24 mx-auto rounded-full overflow-hidden bg-center bg-cover" style={{ backgroundImage: `url(${profilePicUrl})` }}></div>
               {phoneNumber && <div className="text-center mt-3 text-3xl font-semibold">{phoneNumber}</div>}
               <h2 className="text-center">O Whatsapp informado está sendo espionado</h2>
 
-              {/* Loader */}
               <div className="w-72 h-5 mx-auto mt-3 border border-gray-300 rounded overflow-hidden">
                 <div className="h-full bg-green-500" style={{ width: `${loadingPercentage}%` }}></div>
               </div>
-              <div className="text-center mt-2 mb-10">{loadingMessage}</div>
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-5">
-                <Card className="bg-emerald-400">
+              <div className="text-center mt-2 mb-5">{loadingMessage}</div>
+
+              {loadingPercentage >= 90 && (
+                <div className="text-center">
+                  <Button className="p-8 font-semibold animate-bounce" onClick={handleButtonClick}>
+                    <IoIosUnlock className="size-6" />
+                    <p className="ml-2 text-base">Quero acessar as informações</p>
+                  </Button>
+                </div>
+              )}
+
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-5 mb-10">
+                <Card className="bg-emerald-200">
                   <CardContent>
-                    <h2 className="text-lg font-bold">5</h2>
-                    <p>Conversas suspeitas​</p>
-                    <h3>*Conversas contendo algum contexto sexual .​</h3>
+                    <AiFillMessage className="m-auto size-14 text-emerald-600 mt-2 mb-2" />
+                    <h2 className="text-red-600 font-bold text-2xl text-center">{renderValueOrSpinner(suspectConversations)}</h2>
+                    <p className="text-center text-emerald-600 font-bold text-base">Conversas suspeitas​</p>
+                    <h3 className="text-center font-light text-sm">*Conversas contendo algum contexto sexual .​</h3>
                   </CardContent>
                 </Card>
-                <Card className="bg-emerald-400">
+                <Card className="bg-emerald-200">
                   <CardContent>
-                    <h2 className="text-lg font-bold">2</h2>
-                    <p>Imagens com nudez</p>
+                    <FaImages className="m-auto size-14 text-emerald-600 mt-2 mb-2" />
+                    <h2 className="text-red-600 font-bold text-2xl text-center">{renderValueOrSpinner(nudeImages)}</h2>
+                    <p className="text-center text-emerald-600 font-bold text-base">Imagens suspeitas​</p>
+                    <h3 className="text-center font-light text-sm">*Imagens identificadas com conteúdos de nudes.​</h3>
                   </CardContent>
                 </Card>
-                <Card className="bg-emerald-400">
+                <Card className="bg-emerald-200">
                   <CardContent>
-                    <h2 className="text-lg font-bold">3</h2>
-                    <p>Contatos suspeitos</p>
+                    <FaMapMarkedAlt className="m-auto size-14 text-emerald-600 mt-2 mb-2" />
+                    <h2 className="text-red-600 font-bold text-2xl text-center">{renderValueOrSpinner(suspectLocations)}</h2>
+                    <p className="text-center text-emerald-600 font-bold text-base">Localizações suspeitas​</p>
+                    <h3 className="text-center font-light text-sm">*Localizações como motel, casa de massagem e pontos de prostituição.​</h3>
                   </CardContent>
                 </Card>
-              </div>
-              {/* Vídeo/Imagem Container */}
-              <div id="vid_640776e0ae8146000919722f" style={{ position: "relative", width: "100%", paddingTop: "56.25%" }}>
-                <img
-                  id="thumb_640776e0ae8146000919722f"
-                  src="https://images.converteai.net/74042c53-dfa7-44e4-8bcb-7ddb468bdd1f/players/640776e0ae8146000919722f/thumbnail.jpg"
-                  style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", objectFit: "cover", display: "block" }}
-                />
-                <div
-                  id="backdrop_640776e0ae8146000919722f"
-                  style={{ position: "absolute", top: 0, width: "100%", height: "100%", WebkitBackdropFilter: "blur(5px)", backdropFilter: "blur(5px)" }}
-                ></div>
               </div>
             </CardContent>
           </Card>
